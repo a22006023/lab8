e4ab646738f8e681b2a38a7c814379de3af97dca
robroche-a22006023 <a22006023@alunos.ulht.pt>
2022-06-13 01:59:17 +0100

    heroku push

--- portfolio/static/portfolio/js/weather.js
+++ portfolio/static/portfolio/js/weather.js
@@ -1,4 +1,17 @@
+    let currentTemp = document.querySelector('.currentTemp')
+    let dataTemp = document.querySelector('.data')
+    let maxTemp = document.querySelector('.maxTemp')
+    let minTemp = document.querySelector('.minTemp')
+    let icon = document.querySelector('.icon')
+
 document.addEventListener('DOMContentLoaded', function() {
-    fetch()
-    document.querySelector('main').innerHTML = count;
+    fetch('https://api.weatherapi.com/v1/forecast.json?key=df409a4dd5564e069f5230424221206&q=Lisbon&days=3&aqi=no&alerts=no')
+    .then(response => response.json())
+    .then(data => {
+            currentTemp.innerHTML = data.current.temp_c + ' º';
+            dataTemp.innerHTML = data.location.name;
+            maxTemp.innerHTML =  data.forecast.forecastday[0].day.maxtemp_c + ' º';
+            minTemp.innerHTML =  data.forecast.forecastday[0].day.mintemp_c + ' º';
+            icon.src = data.current.condition.icon;
+    });
 });
